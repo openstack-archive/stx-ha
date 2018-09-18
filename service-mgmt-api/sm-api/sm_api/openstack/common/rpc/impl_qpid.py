0d929f884e0ca408a29e48e6077a8d5a83120b15
Sun Austin <austin.sun@intel.com>
2018-09-18 16:55:45 +0800

    Fix next issue for Python 2/3 compatible code.
    change x.next() to next(x)
    and
    add __next__() for class
    
    Story: 2003430
    Task: 26519
    
    Change-Id: I4cda6f1d679e05b7f533b94840e0eb561213aa76
    Signed-off-by: Sun Austin <austin.sun@intel.com>

--- service-mgmt-api/sm-api/sm_api/openstack/common/rpc/impl_qpid.py
+++ service-mgmt-api/sm-api/sm_api/openstack/common/rpc/impl_qpid.py
@@ -520,7 +520,7 @@ class Connection(object):
         it = self.iterconsume(limit=limit)
         while True:
             try:
-                it.next()
+                next(it)
             except StopIteration:
                 return
 
